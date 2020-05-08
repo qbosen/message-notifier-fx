61134772e2b5100a48cf6cbaae0f3cfbc06979d2
qiubaisen <abosen@qq.com>
2020-05-08 16:14:25 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.3.71"
+}
+
+group = "top.abosen"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:1.7.20")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
